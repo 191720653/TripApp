b08f0b3a6e4ac5e0127beb230505db24273d3166
pc-hao <191720653@qq.com>
2016-01-10 22:35:19 +0800

    add slidingmenu model

--- src/com/example/tripapp/MainActivity.java
+++ src/com/example/tripapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.tripapp;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +12,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// configure the SlidingMenu  
+        SlidingMenu menu = new SlidingMenu(this);  
+        menu.setMode(SlidingMenu.LEFT);  
+        // 设置触摸屏幕的模式  
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);  
+         menu.setShadowWidthRes(R.dimen.shadow_width);  
+         menu.setShadowDrawable(R.drawable.shadow);  
+  
+        // 设置滑动菜单视图的宽度  
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);  
+        // 设置渐入渐出效果的值  
+        menu.setFadeDegree(0.35f);  
+        /** 
+         * SLIDING_WINDOW will include the Title/ActionBar in the content 
+         * section of the SlidingMenu, while SLIDING_CONTENT does not. 
+         */  
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);  
+        //为侧滑菜单设置布局  
+        menu.setMenu(R.layout.menu_main);
+		
 	}
 
 	@Override
